96c18b86b91196ab1534ffaa6db3ce780aea11fa
Jeff-King <szwjf08@gmail.com>
2020-12-30 22:02:58 +0800

    update some document.

--- R/exports.R
+++ R/exports.R
@@ -16,14 +16,22 @@ wrap_altrep<-function(x){
     C_wrap_altrep(x)
 }
 
-#' Automatically deploy the filysystem
+#' Filesystem management
 #' 
-#' This function will be called once when the package is loaded.
-#' It will mount a filesystem to a temporary directory. The Travel
-#' package relies on the filesystem to work. It is a no-op if the 
-#' filesystem has been mounted.
-#' @examples 
+#' These functions manage the filesystem which Travel is used to
+#' create virtual pointers. You should not call these functions
+#' manually for Travel will automatically mount the filesystem
+#' if it needs one. 
+#' 
+#' @return Character vector or no return value
+#' @examples  
+#' ## Automatically determine the mountpoint
+#' ## and deploy the filesystem
 #' deploy_filesystem()
+#' 
+#' ## Get mountpoint for the current filesystem
+#' get_mountpoint()
+#' @rdname filesystem
 #' @export
 deploy_filesystem <- function(){
     if(is_filesystem_running()){
@@ -32,4 +40,40 @@ deploy_filesystem <- function(){
     mountpoint <- get_temp_mountpoint()
     set_mountpoint(mountpoint)
     run_filesystem()
+}
+
+#' @rdname filesystem
+#' @export
+get_mountpoint<- function(){
+    C_get_mountpoint()
+}
+
+#' @rdname filesystem
+#' @export
+set_mountpoint <- function(path){
+    if(is_filesystem_running()){
+        stop("The filesystem is running, you cannot change the mountpoint")
+    }
+    C_set_mountpoint(validate_path(path))
+}
+
+#' @rdname filesystem
+#' @export
+run_filesystem <- function(){
+    validate_path(get_mountpoint())
+    C_run_filesystem_thread()
+}
+
+#' @rdname filesystem
+#' @export
+stop_filesystem <- function(){
+    if(is_filesystem_running()){
+        C_stop_filesystem_thread()
+    }
+}
+
+#' @rdname filesystem
+#' @export
+is_filesystem_running<-function(){
+    C_is_filesystem_running()
 }
